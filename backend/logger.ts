8c7066df93a9a5f20a437b3e7890c659f62fb0f7
Julian Toscani <julian_toscani@gmx.de>
2023-02-20 20:45:34 +0100

    2 add dev deps to backend (#2)
    * ➕ winston, ts-node, nodemon
    
    * ✨ add logger
    
    ---------
    
    Co-authored-by: Julian Toscani <julian.toscani@rtl.com>

---
+++ backend/logger.ts
@@ -0,0 +1,36 @@
+import { createLogger, format, transports } from "winston";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const LOGGER_TIMESTAMP_FORMAT = process.env.LOGGER_ERROR_FILE ?? 'YYYY-MM-DD HH:mm:ss';
+const LOGGER_ERROR_FILE = process.env.LOGGER_ERROR_FILE ?? 'error.log';
+const LOGGER_COMBINED_FILE = process.env.LOGGER_COMBINED_FILE ?? 'combined.log';
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp({
+      format: LOGGER_TIMESTAMP_FORMAT
+    }),
+    format.errors({ stack: true }),
+    format.splat(),
+    format.json()
+  ),
+  defaultMeta: { service: 'todo-backend' },
+  transports: [
+    new transports.File({ filename: LOGGER_ERROR_FILE, level: 'error' }),
+    new transports.File({ filename: LOGGER_COMBINED_FILE })
+  ]
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new transports.Console({
+    format: format.combine(
+      format.colorize(),
+      format.simple()
+    )
+  }));
+}
+
+export default logger;
--- backend/main.ts
+++ backend/main.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import app from "./server";
 import { connectToDb } from "./db";
+import logger from "./logger";
 
 dotenv.config();
 
@@ -8,10 +9,10 @@ const PORT = process.env.PORT || 3001;
 const DB_URL = process.env.DB_URL || "";
 const DB_NAME = process.env.DB_NAME || "todos";
 
-console.log(DB_NAME, DB_URL)
+connectToDb(DB_URL, DB_NAME)
+  .then(startServer)
+  .catch(logger.error);
 
-connectToDb(DB_URL, DB_NAME).then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server started at ${PORT}`)
-    })
-}).catch(err => console.error(err))
+function startServer() {
+  app.listen(PORT, () => logger.info(`Server started at ${PORT}`));
+}
