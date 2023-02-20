446c076f7fa169cb9d3b5beee355af9e4d01cb48
Julian Toscani <julian.toscani@rtl.com>
2023-02-20 22:51:48 +0100

    ✨ check for schema update request

--- backend/db.ts
+++ backend/db.ts
@@ -1,17 +1,23 @@
 import { Collection, MongoClient, ServerApiVersion } from "mongodb";
 import { ObjectId } from "bson";
 import dotenv from "dotenv";
+import { setTodoSchema } from "./models/ToDo.model";
 
 let client: MongoClient;
 
 dotenv.config();
 
 const DB_URL = process.env.DB_URL || "";
+const DB_UPDATE_SCHEMA = process.env.DB_UPDATE_SCHEMA ?? "no-update";
 const DB_NAME = process.env.DB_NAME || "todos";
 
 export async function connectToDb() {
     client = new MongoClient(DB_URL, { serverApi: ServerApiVersion.v1 });
     await client.connect();
+
+    if(DB_UPDATE_SCHEMA === "update"){
+        await setTodoSchema();
+    }
 }
 
 export function getDb() {
