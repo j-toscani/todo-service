9bf67a12697c1edc8672305f3140336db272426a
Julian Toscani <julian.toscani@rtl.com>
2023-02-28 16:12:52 +0100

    ✨ add api composable

---
+++ frontend/src/hooks/toDoApi.ts
@@ -0,0 +1,69 @@
+import { ApiToDo } from "../types/ToDo.interface";
+
+const BACKEND_URL = "http://localhost:3001";
+export type ApiResult<T> = { data: T | null; error: Error | null };
+
+function useToDoApi<T = ApiToDo>() {
+  return {
+    deleteToDo(id: string) {
+      return fetchFromApi<T>(`${BACKEND_URL}/todo/${id}`, {
+        method: "DELETE",
+      });
+    },
+    createToDo(data: Partial<ApiToDo>) {
+      return fetchFromApi<T>(`${BACKEND_URL}/todo/new`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ data }),
+      });
+    },
+    updateToDo(data: ApiToDo, id: string) {
+      return fetchFromApi<T>(`${BACKEND_URL}/todo/${id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ data }),
+      });
+    },
+    getToDos(query?: string) {
+      return fetchFromApi<T[]>(
+        `${BACKEND_URL}/todo${query ? `?query=${query}` : ""}`
+      );
+    },
+    getToDo(id: string) {
+      return fetchFromApi<T>(`${BACKEND_URL}/todo/${id}`);
+    },
+  };
+}
+
+async function fetchFromApi<T>(
+  ...options: Parameters<typeof fetch>
+): Promise<ApiResult<T>> {
+  const response = await fetch(...options);
+
+  const result = {
+    data: null,
+    error: null,
+  };
+
+  try {
+    if (!response.ok) {
+      throw new Error(
+        `Response from was not ok: [${response.status}] ${
+          options[1]?.method ?? "GET"
+        }: ${options[0]}`
+      );
+    }
+
+    result.data = await response.json();
+  } catch (error: any) {
+    result.error = error;
+  }
+
+  return result;
+}
+
+export default useToDoApi;
