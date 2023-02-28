05aea015bd3f3ee96cb4396f5c61574f3ef6d829
Julian Toscani <julian.toscani@rtl.com>
2023-02-28 16:12:34 +0100

    🐛 use correct error middleware

--- backend/middleware/handleError.ts
+++ backend/middleware/handleError.ts
@@ -1,12 +1,14 @@
+import { NextFunction } from "connect";
 import { Request, Response } from "express";
 import { ApiError, BadRequestError, InternalServerError } from "../lib/ApiError";
 import { sendError } from "../lib/responseSender";
 import logger from "../logger";
 
 export default function handleError(
-    error: Error | ApiError,
+    error: Error,
     req: Request,
     res: Response,
+    next: NextFunction
   ) {
     if ((error.message = "Document failed validation")) {
       error = new BadRequestError(error.message);
