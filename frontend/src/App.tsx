96232b7fefac2cc0065a1787d3000b3ac05f60d3
Julian Toscani <julian.toscani@rtl.com>
2023-02-28 16:15:21 +0100

    🔥 remove testing code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,15 @@
 import { Container } from "@mantine/core";
-import { useEffect, useState } from "react";
 import TodoForm from "./components/TodoForm";
 import useToDoApi from "./hooks/toDoApi";
-import { ApiToDo } from "./types/ToDo.interface";
 
 function App() {
-  const { getToDos, createToDo } = useToDoApi();
-  const [toDos, setTodos] = useState<ApiToDo[]>([]);
-
-  useEffect(() => {
-    fetchTodos()
-  }, []);
-
-  async function fetchTodos() {
-    const { data } = await getToDos();
-    setTodos(data ?? []);
-  }
-
-  async function handleSubmit(values: Partial<ApiToDo>) {
-    try {
-      await createToDo(values);
-      await fetchTodos();
-    } catch (error) {
-      console.error(error)
-    }
-  }
+  const { createToDo } = useToDoApi();
 
   return (
     <div>
       <Container size="xs">
-        <TodoForm handleSubmit={handleSubmit}/>
+        <TodoForm handleSubmit={createToDo}/>
       </Container>
-      <ul>
-        {toDos.map((todo, index) => (
-          <li key={`${todo.title}-${index}`}>{todo.title}</li>
-        ))}
-      </ul>
     </div>
   );
 }
